fbb0eb4bd12004364caf90af25e4036fed22dff4
DanielAlmazan <almazansellesdaniel@gmail.com>
2024-05-13 18:09:14 +0200

    Fixed SplashScreenActivity

--- app/src/main/java/edu/tasklynx/tasklynxmobile/ui/SplashScreenActivity.kt
+++ app/src/main/java/edu/tasklynx/tasklynxmobile/ui/SplashScreenActivity.kt
@@ -1,4 +1,4 @@
-package edu.tasklynx.tasklynxmobile.ui;
+package edu.tasklynx.tasklynxmobile.ui
 
 import android.content.Intent
 import android.os.Bundle
@@ -6,6 +6,7 @@ import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import edu.tasklynx.tasklynxmobile.databinding.ActivitySplashScreenBinding
+import edu.tasklynx.tasklynxmobile.ui.main.MainActivity
 
 class SplashScreenActivity : AppCompatActivity() {
 	private lateinit var binding: ActivitySplashScreenBinding
@@ -20,7 +21,7 @@ class SplashScreenActivity : AppCompatActivity() {
              startActivity(intent)
              overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
             this.finish()
-         }, 1500)
+         }, 1750)
      }
 
 }
